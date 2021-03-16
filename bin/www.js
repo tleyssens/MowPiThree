fdd3d0ae9ab6d3b42db589cf4ce8ce866a1bf796
Tom Leyssens <tom.leyssens@telenet.be>
2021-03-16 22:39:55 +0100

    Add starting point app + settingsfile

--- bin/www.js
+++ bin/www.js
@@ -11,18 +11,17 @@ let nmeaFunc = require('../controllers/nmeaFunc')
 /**
  * Module dependencies.
  */
-var app = require('../app');
-var debug = require('debug')('mowpi52-3b2socket2:server');
-var http = require('http');
-const { monitorEventLoopDelay } = require('perf_hooks');
+var app = require('../app')
+var http = require('http')
+const { monitorEventLoopDelay } = require('perf_hooks')
 if (s.plaats === "pa") {var mower = require('../lib/Mower') }
 if (s.plaats === "tom") {var mower = require('../lib/MowerTom') }
 
 /**
  * Get port from environment and store in Express.
  */
-var port = normalizePort(process.env.PORT || '3333');
-app.set('port', port);
+var port = normalizePort(process.env.PORT || '3333')
+app.set('port', port)
 
 /**
  * Connect settings to app
@@ -35,27 +34,26 @@ app.set('port', port);
 /**
  * Create HTTP server.
  */
-//cc:1_OpstartenServer#1;createserver
-var server = http.createServer(app);
+var server = http.createServer(app)
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
 
 /**
- * Create socket.io server (Tom: bijgezet)
+ * Create socket.io server
  * wat heb ik geleerd: 
  *      # dit geeft een module terug io = require( "socket.io" )
  *      # dit geeft een object terug io = require( "socket.io" )()
  *   te testen met console.log(Object.getOwnPropertyNames(io))
  * https://stackoverflow.com/questions/24609991/using-socket-io-in-express-4-and-express-generators-bin-www
  */
-var socketApi = require('../socketApi')(s);
-var io = socketApi.io;
-io.attach(server); // Tom: bijgezet
+var socketApi = require('../socketApi')(s)
+var io = socketApi.io
+io.attach(server)
 
 /**
  * Termination
@@ -85,7 +83,7 @@ function exit() {
  * Normalize a port into a number, string, or false.
  */
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  var port = parseInt(val, 10)
 
   if (isNaN(port)) {
     // named pipe
@@ -94,10 +92,10 @@ function normalizePort(val) {
 
   if (port >= 0) {
     // port number
-    return port;
+    return port
   }
 
-  return false;
+  return false
 }
 
 /**
@@ -105,22 +103,22 @@ function normalizePort(val) {
  */
 function onError(error) {
   if (error.syscall !== 'listen') {
-    throw error;
+    throw error
   }
 
   var bind = typeof port === 'string'
     ? 'Pipe ' + port
-    : 'Port ' + port;
+    : 'Port ' + port
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges');
-      process.exit(1);
+      process.exit(1)
       break;
     case 'EADDRINUSE':
       console.error(bind + ' is already in use');
-      process.exit(1);
+      process.exit(1)
       break;
     default:
       throw error;
@@ -134,6 +132,6 @@ function onListening() {
   var addr = server.address();
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+    : 'port ' + addr.port
+  debug('Listening on ' + bind)
 }
